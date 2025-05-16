f4eac5aa09eb523defbbe6e193d89e056ee0d893
Franpa99 <113123346+Franpa99@users.noreply.github.com>
2025-05-16 00:15:57 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,16 +8,11 @@ let isLoading = false;
 let minnieMode = false;
 
 const minnieImages = [
-    '2d721226-cddf-4036-bb5b-32f47f35ccd9.jpg',
     '3c361ba7-1d84-4aa5-b889-a2a4306390ae.jpg',
-    '46b7ec6d-b617-4e00-b421-beaf7dd02bc4.jpg',
     '73523c87-7ab5-4aca-83e8-0f92b707b339.jpg',
     '74945961-ac6b-406a-854b-be6a39a9b262.jpg',
     '90eab1c0-a81a-425b-af1d-c3162f65716f.jpg',
-    'aa51f1f6-e131-42b5-bb31-6f3ab0d9f815.jpg',
-    'dd08954f-5d8a-4c1d-862a-3a3422f782be.jpg',
-    'e4fa4d78-4660-45f9-a74c-f0dc1f8490a8.jpg',
-    'f19e3d6b-6262-4b1c-b09e-4d10642788c1.jpg'
+    'aa51f1f6-e131-42b5-bb31-6f3ab0d9f815.jpg'
 ];
 
 function formatBreedName(url) {
