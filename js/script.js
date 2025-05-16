afd431bc396ff93f775a0f21bf5b8f81c127e74c
Franpa99 <113123346+Franpa99@users.noreply.github.com>
2025-05-15 23:19:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
 const dogImage = document.getElementById('dogImage');
 const audio = document.getElementById('audio');
+const loader = document.getElementById('loader');
+
 let isPlaying = false;
+let isLoading = false;
+
 async function fetchRandomDogImage() {
+    if (isLoading) return;
+
+    isLoading = true;
+    loader.classList.remove('hidden');
+
+    dogImage.style.opacity = 0;
+    
     try {
         const response = await fetch('https://dog.ceo/api/breeds/image/random');
         const data = await response.json();
         const imageUrl = data.message;
-        dogImage.src = imageUrl;
+
+        setTimeout(() => {
+            dogImage.src = imageUrl;
+
+            dogImage. style.opacity = 1;
+            loader.classList.add('hidden');
+            isLoading = false;
+        }, 500);
     } catch (error) {
         console.error('Error al cargar la imagen del perro:', error);
+        loader.textContent = 'Error cargando la imagen.';
+        setTimeout(() => {
+            loader.classList.add('hidden');
+            loader.textContent = 'Cargando...';
+            isLoading = false;
+        }, 2000);
     }
 }
 
